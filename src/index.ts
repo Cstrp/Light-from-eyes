da40935c83901dba63e90dfd855e1a0d5782ad77
Valery Nosareu <86977555+Cstrp@users.noreply.github.com>
2022-06-13 18:14:00 +0300

    in process..

--- src/index.ts
+++ src/index.ts
@@ -10,45 +10,87 @@ const colors: Array<string> = [
     'orange',
     'purple']
 
-type T0 = Exclude<'a' | 'b' | 'c', 'a'>
-const a: T0 = 'b'
+const classColor: Array<string> = [
+    'red',
+    'green',
+    'blue',
+    'yellow',
+    'black',
+    'pink',
+    'orange',
+    'purple'
+]
 
+const container: HTMLElement = document.querySelector('.container')
+const blocks = document.querySelector('.block__container')
 
-function block() {
+function block(): void {
     const arr: Array<HTMLElement> = []
     for (let i = 0; i < 24; i++) {
-        const container = document.createElement('div')
-        container.classList.add('block')
-        container.style.backgroundColor = colors[Math.floor(
-            Math.random() * colors.length)]
-        arr.push(container)
+        const block = document.createElement('div')
+        block.classList.add('block')
+        arr.push(block)
     }
     arr.forEach(item => {
-        document.body.append(item)
+        blocks.append(item)
+    })
+    return arr.forEach((item) => {
+        item.classList.add(classColor[[Math.floor(Math.random() * classColor.length)]])
     })
 }
 
-function RedButton() {
-    const button = document.createElement('button')
-    button.classList.add('RedBtn')
-    button.innerHTML = '000'
-    document.body.append(button)
+
+function RedButton(): void {
+    const button: HTMLButtonElement = document.createElement('button')
+    button.classList.add('btn')
+    button.innerHTML = 'Create'
+    container.append(button)
     button.addEventListener('click', () => {
         block()
     })
 }
 
-function filterColor() {
-    const input = document.createElement('input')
+RedButton()
+
+function form() {
+    const input: HTMLInputElement = document.createElement('input')
     input.classList.add('input')
-    document.body.append(input)
-    // input.addEventListener('input', (evt) => {
-    //     block(filterBlock(evt.target.value, colors), result)
-    // })
+
 }
 
-filterColor()
-RedButton()
+function urlParser(url: string): Array<Object> {
+    const result = []
+    url.split('?')[1].split('&').reduce((acc, item) => {
+        const parse = item.split('=')
+        acc[parse[0]] = parse[1]
+        return result.push(acc)
+    }, {})
+    return result
+}
+
+console.log(urlParser('1'))
+
+
+// url.split(‘?’)[1]
+//     .split(‘&’)
+// .reduce((acc, elem) =>{
+//     const pair = elem.split(‘=‘)
+//     acc[pair[0]] = pair[1]
+//     return acc
+// }, {})
+
+
+// function filterColor() {
+//     const input = document.createElement('input')
+//     input.classList.add('input')
+//     document.body.append(input)
+// input.addEventListener('input', (evt) => {
+//     block(filterBlock(evt.target.value, colors), result)
+// })
+// }
+
+// filterColor()
+// RedButton()
 
 
 
