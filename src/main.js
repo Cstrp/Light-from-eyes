41e74b8f147d060fd26f0adb91a215aac042da6a
Valery Nosareu <86977555+Cstrp@users.noreply.github.com>
2022-06-15 01:52:21 +0300

    fixed func

--- src/main.js
+++ src/main.js
@@ -20,29 +20,6 @@ class FluidTypography {
     const minRem = max / baseFontSize
     return { maxRem, minRem }
   }
-
-  fontSize() {
-    const width = document.documentElement.offsetWidth
-    let rem = this.minRem
-
-    if (width > this.minVW && width < this.maxVW) {
-      rem =
-        this.minRem +
-        ((this.maxRem - this.minRem) * (width - this.minVW)) /
-          (this.maxVW - this.minVW)
-    }
-
-    if (width > this.maxVW) {
-      rem = this.maxRem
-    }
-
-    document.documentElement.style = `font-size: ${rem}rem`
-  }
-
-  resizeHandler() {
-    this.fontSize()
-    window.addEventListener('resize', this.fontSize.bind(this))
-  }
 }
 
-new FluidTypography(640, 1280, 17.5, 22).resizeHandler()
+new FluidTypography(640, 1280, 17.5, 22)
