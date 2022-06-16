6a95525a230260f58d12f6de498a78a4e4cbb8b5
Valery Nosareu <86977555+Cstrp@users.noreply.github.com>
2022-06-16 10:17:38 +0300

    test

--- src/main.js
+++ src/main.js
@@ -1,48 +1,14 @@
 import './view/styles/index.scss'
 
-class FluidTypography {
-  constructor(minVW, maxVW, minFontSize, maxFontSize) {
-    this.minVW = minVW
-    this.maxVW = maxVW
-    this.minFontSize = minFontSize
-    this.maxFontSize = maxFontSize
-    this.maxRem = this.computeRem().maxRem
-    this.minRem = this.computeRem().minRem
-  }
-
-  computeRem() {
-    const body = document.documentElement
-    const properties = window.getComputedStyle(body)
-    const baseFontSize = properties.fontSize.replace(/px/, '')
-    const max = Math.max(this.minFontSize, baseFontSize)
-    const relativeMax = (this.maxFontSize * max) / this.minFontSize
-    const maxRem = relativeMax / baseFontSize
-    const minRem = max / baseFontSize
-    return { maxRem, minRem }
-  }
-
-  fontSize() {
-    const width = document.documentElement.offsetWidth
-    let rem = this.minRem
-
-    if (width > this.minVW && width < this.maxVW) {
-      rem =
-        this.minRem +
-        ((this.maxRem - this.minRem) * (width - this.minVW)) /
-          (this.maxVW - this.minVW)
+function headerActive() {
+  const header = document.querySelector('.header')
+  window.onscroll = () => {
+    if (window.scrollY > 50) {
+      header.classList.add('active')
+    } else {
+      header.classList.remove('.active')
     }
-
-    if (width > this.maxVW) {
-      rem = this.maxRem
-    }
-
-    document.documentElement.style = `font-size: ${rem}rem`
-  }
-
-  resizeHandler() {
-    this.fontSize()
-    window.addEventListener('resize', this.fontSize.bind(this))
   }
 }
 
-new FluidTypography(320, 1440, 12, 55).resizeHandler()
+headerActive()
