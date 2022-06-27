8b533365e27e17f0d0d32d6521b221fd226f8d6d
Cstrp <valery.nosareu@gmail.com>
2022-06-27 15:39:57 +0300

    fixed bug

--- src/main.js
+++ src/main.js
@@ -1,21 +1,58 @@
 import './view/styles/index.scss'
 
-function headerActive() {
-  const header = document.querySelector('.header')
-  window.onscroll = () => {
-    if (window.scrollY > 50) {
-      header.classList.add('active')
-    } else {
-      header.classList.remove('.active')
-    }
-  }
-}
-
-headerActive()
-
 const burgerMenu = document.querySelector('.header__nav-mobile')
 const burgerBtn = document.querySelector('.header__nav_mobile-menu')
 burgerBtn.addEventListener('click', (evt) => {
   burgerBtn.classList.toggle('active')
   burgerMenu.classList.toggle('active')
+})
+
+const next = document.querySelectorAll('.intro__next')
+const prev = document.querySelectorAll('.intro__prev')
+const container = document.querySelectorAll('.intro__wrapper')
+const dots = document.querySelectorAll('.dot')
+let index = 0
+
+const slider = () => {
+  for (let i = 0; i < container.length; i++) {
+    // console.log(i)
+    container[i].classList.add('active')
+  }
+  container[index].classList.remove('active')
+  for (let i = 0; i < dots.length; i++) {
+    if (i === 0 || 3 || 6) {
+      dots[i].classList.add('active')
+    }
+  }
+}
+
+// dots.forEach((btn) => {
+//   btn.addEventListener('click', () => {
+//     btn.classList.add('active')
+//   })
+//   slider()
+// })
+
+// dots[i].className = dots[i].className.replace(" active", "");
+// dots[slideIndex-1].className += " active";
+next.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    if (index + 1 === container.length) {
+      index = 0
+    } else {
+      index++
+    }
+    slider()
+  })
+})
+
+prev.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    if (index - 1 === -1) {
+      index = container.length - 1
+    } else {
+      index--
+    }
+    slider()
+  })
 })
